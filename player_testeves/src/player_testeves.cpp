05200c1d1b9969f50d7ba0ec40cb1d9197aed335
tiagoesteves <tiagoesteves@followinspiration.pt>
2016-12-21 12:52:49 +0000

    improve

--- player_testeves/src/player_testeves.cpp
+++ player_testeves/src/player_testeves.cpp
@@ -18,7 +18,7 @@ using namespace ros;
  */
 class MyPlayer: public rwsfi2016_libs::Player
 {
-  public: 
+public:
 
     /**
      * @brief Constructor, nothing to be done here
@@ -29,34 +29,34 @@ class MyPlayer: public rwsfi2016_libs::Player
 
     void play(const rwsfi2016_msgs::MakeAPlay& msg)
     {
-      //Custom play behaviour. Now I will win the game
+        //Custom play behaviour. Now I will win the game
 
-      //Behaviour follow the closest prey
-	double dist_min = 1000;
-	int angleMin = 0;
-	double dist = 0;
-	for (int pl=0; pl<preys_team->players.size(); pl++) {
-		dist = getDistanceToPlayer(preys_team->players[pl]);
-	if (dist_min < dist) {
-		angleMin = pl;
-		dist_min = dist;
-	}
-	}
-double dist_min_hunter = 1000;
-double dist_hunter = 0;
-int angleMinHunter = 0;
-	for (int pl=0; pl<hunters_team->players.size(); pl++) {
-		dist_hunter = getDistanceToPlayer(hunters_team->players[pl]);
-	if (dist_min_hunter < dist_hunter) {
-		angleMinHunter = pl;
-		dist_min_hunter = dist_min;
-	}
-	}
-if (dist_min_hunter < dist_min) {
-move(msg.max_displacement, getAngleToPLayer(hunters_team->players[angleMinHunter])-M_PI);
-} else {
-      move(msg.max_displacement, getAngleToPLayer(preys_team->players[angleMin]));
-}
+        //Behaviour follow the closest prey
+        double dist_min = 100000;
+        int angleMin = 0;
+        double dist = 0;
+        for (int pl=0; pl < preys_team->players.size(); pl++) {
+            dist = getDistanceToPlayer(preys_team->players[pl]);
+            if (dist < dist_min) {
+                angleMin = pl;
+                dist_min = dist;
+            }
+        }
+        double dist_min_hunter = 100000;
+        double dist_hunter = 0;
+        int angleMinHunter = 0;
+        for (int pl=0; pl < hunters_team->players.size(); pl++) {
+            dist_hunter = getDistanceToPlayer(hunters_team->players[pl]);
+            if (dist_hunter < dist_min_hunter) {
+                angleMinHunter = pl;
+                dist_min_hunter = dist_hunter;
+            }
+        }
+        if (dist_min_hunter < dist_min) {
+            move(msg.max_displacement, getAngleToPLayer(hunters_team->players[angleMinHunter])+M_PI/3);
+        } else {
+            move(msg.max_displacement, getAngleToPLayer(preys_team->players[angleMin]));
+        }
     }
 };
 
@@ -69,18 +69,18 @@ move(msg.max_displacement, getAngleToPLayer(hunters_team->players[angleMinHunter
  */
 int main(int argc, char** argv)
 {
-  // ------------------------
-  //Replace this with your name
-  // ------------------------
-  string my_name = "testeves";
-  string my_pet = "/cheetah";
+    // ------------------------
+    //Replace this with your name
+    // ------------------------
+    string my_name = "testeves";
+    string my_pet = "/cheetah";
 
-  //initialize ROS stuff
-  ros::init(argc, argv, my_name);
+    //initialize ROS stuff
+    ros::init(argc, argv, my_name);
 
-  //Creating an instance of class MyPlayer
-  MyPlayer my_player(my_name, my_pet);
+    //Creating an instance of class MyPlayer
+    MyPlayer my_player(my_name, my_pet);
 
-  //Infinite spinning (until ctrl-c)
-  ros::spin();
+    //Infinite spinning (until ctrl-c)
+    ros::spin();
 }
